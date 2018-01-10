a496c39ab0d0fa57267973a18125465b3dbc79e4
Vinicius March <viniciusmarch01@gmail.com>
2018-01-10 20:28:46 -0200

    First commit of the study revision

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+//criando o servidor
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if(error){
+      console.log("Unable to append to server.log");
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('ops.hbs', {
+//     pageTitle: 'Oh no Page',
+//     sorryMessage: "Ops. This page doesn't exist"
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+//configurações
+//Um handler, que executa a funçao do segundo argumento, quando a url do primeiro argumento for acessada
+app.get('/', (req, res) => {
+  // res.send("<h1>Hello, Express!</h1>");
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeText: 'Welcome to my first node website!!',
+    likes: ['Women', ' Stories', ' Travel']
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  })
+});
+
+
+//metodo get
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Ops! The page is not here D:'
+  });
+});
+
+//req -> informacoes do html (headers, body...)
+//res -> açoes de reseposta (rever no site)
+
+//Fazendo o servidor escutar
+app.listen(3000, () => {
+  console.log('Server listening from port 3000');
+});
